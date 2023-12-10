6f74922cfc76db9b3930692e1fc7094f69d1e26c
erkang <145332915+288017@users.noreply.github.com>
2023-12-10 11:27:07 +0800

    Add files via upload

---
+++ 3.1/4.1.6/2.1.6/FileName.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	string str;
+	cout << "Please input some text: ";
+	getline(cin, str);
+
+	transform(str.begin(), str.end(), str.begin(), ::tolower); 
+	int cnt[256] = {};
+	for (int i = 0; i < str.size(); i++) {
+		cnt[(int)str[i]]++;
+	}
+	
+	for (int i = 0; i < 256; i++) {
+		if (cnt[i] != 0) {
+			cout << "the number of " << (char)i << ": " << cnt[i] << endl;
+		}
+	}
+}
